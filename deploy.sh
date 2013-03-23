9f1aa07128b92f3ef53a8343447fcd019aa40ea3
Danny Arnold <danny.arnold@student.tu-freiberg.de>
2013-03-23 20:40:27 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,15 @@
 #!/bin/sh -xe
 
 brunch build
+codo --private
 cp -r public ../
+cp -r doc ../
 cp -r node_modules ../
 git stash -u
 git checkout gh-pages
 rm -rf *
 cp -r ../public/ .
+cp -r ../doc .
 git add --all
 git commit -m 'update site'
 git push
@@ -15,3 +18,4 @@ git stash pop
 cp -r ../node_modules .
 rm -rf ../public
 rm -rf ../node_modules
+rm -rf ../doc
